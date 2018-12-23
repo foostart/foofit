add1daec6f30d4ca00b15ce8375ee6f6a177e259
HuynhTrungTruc <33856685+HuynhTrungTruc@users.noreply.github.com>
2018-12-23 11:31:48 +0700

    Revert "Update W3C"
    This reverts commit af6643622fdda82d2fb466c1cb19be270e1efc21.

--- 172/172-content.php
+++ 172/172-content.php
@@ -23,52 +23,48 @@ $url_path = str_replace('\\', '/', $url_path);
                 <div class="tabs tab3">
                     <div class="row">
                         <div class="col-md-4 col-sm-6 col-xs-12">
-                            <ul>
-                                <li>
-                                    <div class="cube">
-                                        <div class="active-state">
-                                            <br>
-                                            <p>Trang bị cho sinh viên kiến thức và kỹ năng để trở thành nhà phát triển phần mềm chuyên nghiệp.</p>
-                                        </div>
-                                        <div class="default-state">
-                                            <h1>Ngành Công nghệ thông tin</h1>
-                                        </div>
-                                        <div class="button_more"> <a href="#" class="more">Xem thêm</a></div>
+                            <li>
+                                <div class="cube">
+                                    <div class="active-state">
+                                        <br>
+                                        <p>Trang bị cho sinh viên kiến thức và kỹ năng để trở thành nhà phát triển phần mềm chuyên nghiệp.</p>
                                     </div>
-                                </li>
-                            </ul>
+                                    <div class="default-state">
+                                        <h1>Ngành Công nghệ thông tin</h1>
+                                    </div>
+                                    <div class="button_more"> <a href="#" class="more">Xem thêm</a></div>
+                                </div>
+
+                            </li>
                         </div>
                         <div class="col-md-4 col-sm-6 col-xs-12">
-                            <ul>
-                                 <li class="second-li">
-                                    <div class="cube">
-                                        <div class="active-state">
-                                            <br><p>Sinh viên có khả năng nghiên cứu, thiết kế, phát triển và triển khai các ứng dụng về các công nghệ Mạng máy tính.</p>
-                                        </div>
-                                        <div class="default-state">
-                                            <h1>Ngành Truyền thông và Mạng máy tính</h1>
-                                        </div>
-                                        <div class="button_more"> <a href="#" class="more">Xem thêm</a></div>
+                            <li class="second-li">
+                                <div class="cube">
+                                    <div class="active-state">
+                                        <br><p>Sinh viên có khả năng nghiên cứu, thiết kế, phát triển và triển khai các ứng dụng về các công nghệ Mạng máy tính.</p>
                                     </div>
-                                </li>
-                            </ul>
-                               
+                                    <div class="default-state">
+                                        <h1>Ngành Truyền thông và Mạng máy tính</h1>
+                                    </div>
+                                    <div class="button_more"> <a href="#" class="more">Xem thêm</a></div>
+                                </div>
+
+                            </li>
                         </div>
                         <div class="col-md-4 col-sm-12 col-xs-12">
-                            <ul>
-                                <li class="third-li">
-                                    <div class="cube">
-                                        <div class="active-state">
-                                            <br>
-                                            <p>Sinh viên có kiến thức nền tảng và chuyên sâu trong các lĩnh vực thiết kế quảng cáo, phục vụ cho rất nhiều mục đích như: truyền thông, quảng cáo, ...</p>
-                                        </div>
-                                        <div class="default-state">
-                                            <h1>Ngành Thiết kế <br> Đồ họa</h1>
-                                        </div>
-                                        <div class="button_more"> <a href="#" class="more">Xem thêm</a></div>
+                            <li class="third-li">
+                                <div class="cube">
+                                    <div class="active-state">
+                                        <br>
+                                        <p>Sinh viên có kiến thức nền tảng và chuyên sâu trong các lĩnh vực thiết kế quảng cáo, phục vụ cho rất nhiều mục đích như: truyền thông, quảng cáo, ...</p>
                                     </div>
-                                </li>
-                            </ul>      
+                                    <div class="default-state">
+                                        <h1>Ngành Thiết kế <br> Đồ họa</h1>
+                                    </div>
+                                    <div class="button_more"> <a href="#" class="more">Xem thêm</a></div>
+                                </div>
+
+                            </li>
                         </div>
                     </div>
 
@@ -90,53 +86,49 @@ $url_path = str_replace('\\', '/', $url_path);
                 <div class="tabs tab3">
                     <div class="row">
                         <div class="col-md-4 col-sm-6 col-xs-12">
-                            <ul>
-                                <li >
-                                    <div class="cube">
-                                        <div class="active-state">
-                                            <br>
-                                            <p>Trang bị cho học sinh kiến thức và kỹ năng để làm việc trong lĩnh vực phát triển và kiểm thử phần mềm và web.</p>
-                                        </div>
-                                        <div class="default-state">
-                                            <h1>Ngành Tin học<br> Ứng dụng</h1>
-                                        </div>
-                                        <div class="button_more"> <a href="#" class="more">Xem thêm</a></div>
+                            <li >
+                                <div class="cube">
+                                    <div class="active-state">
+                                        <br>
+                                        <p>Trang bị cho học sinh kiến thức và kỹ năng để làm việc trong lĩnh vực phát triển và kiểm thử phần mềm và web.</p>
                                     </div>
-                                </li>
-                            </ul> 
+                                    <div class="default-state">
+                                        <h1>Ngành Tin học<br> Ứng dụng</h1>
+                                    </div>
+                                    <div class="button_more"> <a href="#" class="more">Xem thêm</a></div>
+                                </div>
+
+                            </li>
                         </div>
                         <div class="col-md-4 col-sm-6 col-xs-12">
-                            <ul>
-                                <li class="second-li">
-                                    <div class="cube">
-                                        <div class="active-state">
-                                            <br>
-                                            <p>Học sinh có khả năng triển khai các ứng dụng về các công nghệ Mạng máy tính như cài đặt, quản trị mạng.</p>
-                                        </div>
-                                        <div class="default-state">
-                                            <h1>Ngành Truyền thông và Mạng máy tính</h1>
-                                        </div>
-                                        <div class="button_more"> <a href="#" class="more">Xem thêm</a></div>
+                            <li class="second-li">
+                                <div class="cube">
+                                    <div class="active-state">
+                                        <br>
+                                        <p>Học sinh có khả năng triển khai các ứng dụng về các công nghệ Mạng máy tính như cài đặt, quản trị mạng.</p>
                                     </div>
-                                </li>
-                            </ul>
+                                    <div class="default-state">
+                                        <h1>Ngành Truyền thông và Mạng máy tính</h1>
+                                    </div>
+                                    <div class="button_more"> <a href="#" class="more">Xem thêm</a></div>
+                                </div>
 
+                            </li>
                         </div>
                         <div class="col-md-4 col-sm-12 col-xs-12">
-                            <ul>
-                                <li class="third-li">
-                                    <div class="cube">
-                                        <div class="active-state">
-                                            <br>
-                                            <p>Cung cấp các kiến thức về thiết kế đồ họa và công nghệ thông tin đa phương tiện.</p>
-                                        </div>
-                                        <div class="default-state">
-                                            <h1>Ngành Truyền thông đa phương tiện</h1>
-                                        </div>
-                                        <div class="button_more"> <a href="#" class="more">Xem thêm</a></div>
+                            <li class="third-li">
+                                <div class="cube">
+                                    <div class="active-state">
+                                        <br>
+                                        <p>Cung cấp các kiến thức về thiết kế đồ họa và công nghệ thông tin đa phương tiện.</p>
                                     </div>
-                                </li>
-                            </ul>
+                                    <div class="default-state">
+                                        <h1>Ngành Truyền thông đa phương tiện</h1>
+                                    </div>
+                                    <div class="button_more"> <a href="#" class="more">Xem thêm</a></div>
+                                </div>
+
+                            </li>
                         </div>
                     </div>
 
